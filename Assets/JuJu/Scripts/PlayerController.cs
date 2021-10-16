899df94f86e47556dc630c74fe1dd1de4d6d01d4
JulianDKal <juliandk1357@gmail.com>
2021-10-16 17:37:47 +0200

    hourglass rotation

--- Assets/JuJu/Scripts/PlayerController.cs
+++ Assets/JuJu/Scripts/PlayerController.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Cinemachine;
 
 namespace JuJu {
 
 public class PlayerController : MonoBehaviour
 {
 
-    //values visibile in the inspector
+    //variables visibile in the inspector
     [SerializeField]
     private float moveSpeed = 1;
     [SerializeField]
     private float jumpForce = 5;
+    [SerializeField]
+    private Transform hourglass;
 
     //private variables
     private Rigidbody2D rb;
+    private Camera cam;
+    private Vector2 gravityVector = new Vector2(0, -9.8f);
+    private Quaternion targetAngle = Quaternion.Euler(0, 0, 0);
+    private bool isTurned = false;
+    
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        Physics.gravity = gravityVector;
+        cam = Camera.main;
     }
 
     void Update()
@@ -30,6 +40,31 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            //gravityVector *= -1;
+            //Physics2D.gravity = gravityVector;
+            ChangeRotation();
+            Debug.Log("F Key pressed");
+        }
+
+        hourglass.rotation = Quaternion.Slerp(hourglass.rotation, targetAngle, Time.deltaTime);
+        Debug.Log(isTurned);
+    }
+
+    void ChangeRotation()
+    {
+        if(isTurned == false)
+        {
+            targetAngle = Quaternion.Euler(0,0,180);
+            isTurned = true;
+        }
+        else
+        {
+            targetAngle = Quaternion.Euler(0,0,0);
+            isTurned = false;
+        }
+
     }
 }
 
