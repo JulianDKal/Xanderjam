7d787ae6f944af1793d4b09acf6392a979daf999
JulianDKal <juliandk1357@gmail.com>
2021-10-17 16:22:29 +0200

    implemented cracks

--- Assets/JuJu/Scripts/Crack.cs
+++ Assets/JuJu/Scripts/Crack.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class Crack : MonoBehaviour
 {
-    public Sprite[] crackVersions;
+    ///<summary>
+    ///class for the cracks on the hourglass. When the player presses R while being on top of a crack, the crack's health will decrease 
+    ///causing it to display another image. 
+    ///</summary>
 
-    private void OnCollisionStay2D(Collision2D other) {
-        if(other.collider.CompareTag("Player"))
+    public Sprite[] crackVersions; //An array of the different versions of a crack
+    public GameObject player;
+
+    private int crackHealth; 
+
+    private void Awake() {
+        crackHealth = crackVersions.Length;
+        GetComponent<SpriteRenderer>().sprite = crackVersions[crackHealth - 1];
+        try
         {
-
+             player = GameObject.Find("Player");
+        }
+        catch (System.NullReferenceException)
+        {
+            Debug.LogWarning("Player not active in hierarchy!");
         }
     }
+
+    private void Update() {
+        if(GetComponent<Collider2D>().OverlapPoint(player.transform.position))
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                crackHealth--;
+                if(crackHealth <= 0) {
+                    Destroy(gameObject);
+                    return;
+                    }
+                GetComponent<SpriteRenderer>().sprite = crackVersions[crackHealth -1];
+                Debug.Log(crackHealth);
+            }
+        }
+    }
+
+
 }
