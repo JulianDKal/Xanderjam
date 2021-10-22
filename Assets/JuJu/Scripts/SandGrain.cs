92b09e1c01f0b884917d8e0d98534c6162007881
R3inis <67374883+R3inis@users.noreply.github.com>
2021-10-22 10:52:36 +0300

    Visual/performance improovements
    Note: did have to change the layout of the level

--- Assets/JuJu/Scripts/SandGrain.cs
+++ Assets/JuJu/Scripts/SandGrain.cs
@@ -7,11 +7,27 @@ public class SandGrain : MonoBehaviour
     [SerializeField]
     private float randomRangeX = 2f;
 
+    float time = 3;
+
     private Rigidbody2D rb;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
         float randomForceX = Random.Range(-randomRangeX, randomRangeX);
-        rb.AddForce(new Vector2(randomForceX, 0));
+        rb.AddForce(new Vector2(randomForceX, -2));
+        time -= Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        if(rb.velocity.magnitude < 0.2f && time < 0)
+        {
+            Freeze();
+        }
+    }
+
+    void Freeze()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
     }
 }
